33924f4e573f42557fc6846464676f7863315186
arijohn@pintech.ki <arijohn@pintech.ki>
2022-09-23 18:14:18 +1200

    added 2nd try

---
+++ 2nd try/2-Hi-low/cardgame.py
@@ -0,0 +1,168 @@
+"""
+Ari-John Katia
+High Low GAME
+CSE 210
+"""
+
+#Currently doing all my classes in one file because the current programs on android have trouble trying to read through local modules.
+
+
+import random
+
+def __main__():
+    return
+
+class Cards:
+  """
+  Cards class for generating cards
+  """
+    def __init__(self):
+        self.value = 0
+        self.number = 0
+        
+    def draw(self):
+        value = int(random.randint(1,13))
+        self.value = value
+        return self.value
+        
+# decided to add this just to add a little flair to the game but not too much        
+    def cardname(self):
+      """
+      Displays the card's name instead of the number
+      """
+        if self.value == 11:
+            self.number = "J"
+        elif self.value == 12:
+            self.number = "Q"
+        elif self.value == 13:
+            self.number = "K"
+        elif self.value == 1:
+            self.number = "A"
+        else:
+            self.number = self.value
+            
+        return self.number
+
+
+
+class Director:
+  """
+  Director class for directing the game... and stuff, I think there might have been a template or something for the hi-low game last semester or something, because I have no idea where I got the majority of these codes
+  """
+    
+    def __init__(self):
+        self.cards = []
+        self.playing = True
+        self.score = 300
+        self.answered = False
+        #added this last one for debugging purposes.. okay, not debugging, but I got used to having error messages popping up for when I didn't enter the right stuff so I decided to try to add it into all my codes
+
+            
+    def printcard(self):
+      """
+      prints a card
+      """
+        if self.playing == False:
+            return
+        else:
+            print(f"The Card is: {Cards.cardname(self)}")
+        
+    def printnext(self):
+      """
+      prints the next card
+      """
+        if self.playing == False:
+            return
+        else:
+            print(f"Next card was: {Cards.cardname(self)}")
+        
+    def hi_low(self, last, now):
+      """
+      calculates the high or low by ... calculating the high and low
+      """
+        if not self.playing:
+            return
+        else:
+            if now > last:
+                return "h"
+            elif now < last:
+                return "l"
+        
+    def getansw(self):
+      """
+      recieves the users answer to high or low
+      """
+        if self.playing == False:
+            return
+        else:
+            while self.answered != True:
+                answer = input("High or Low? [h/l]")
+                if answer.lower() == "h" or answer.lower() == "l":
+                    self.answered = True
+                    return answer.lower()
+                else:
+                    print("Please enter valid value")
+            
+    def checkansw(self, x, y):
+      """
+      checks to see if users answer is correct
+      """
+        if self.playing == False:
+            return
+        else:
+            if x == y:
+                self.score = self.score + 100
+            else:
+                self.score = self.score - 75
+            print(f"Current score is: {self.score}")
+    
+    def play(self):
+      """
+      checks to see if player wants to play again... that is if you still have points
+      """
+        if self.playing == False:
+            return
+        else:
+            if self.score > 0:
+                while self.answered != True:
+                    again = input("Play again? [y/n]")
+                    if again.lower() == "y":
+                        self.playing = True
+                        self.answered = True
+                    elif again.lower() == "n":
+                        self.playing = False
+                        self.answered = True
+                    else:
+                        self.answered = False
+                        print("Please enter valid value")
+            else:
+                print("GAME OVER!! \nYou ran out of points")
+    
+    def updates(self):
+      """
+      checks if the program is still running
+      """
+        if self.playing == False:
+            return
+    
+    
+    def start_game(self):
+        
+        while not (self.playing == False or self.score <= 0):
+            self.answered = False
+            current = Cards.draw(self)
+            self.printcard()
+            previous = current
+            y_answer = self.getansw()
+            current = Cards.draw(self)
+            self.printnext()
+            answer = self.hi_low(previous, current)
+            self.checkansw(y_answer, answer)
+            self.answered = False
+            self.play()
+            self.updates()
+
+#only doing this thing because I have no idea how to make this look good
+if __name__ == "__main__":
+    director = Director()
+    director.start_game()
---
+++ 2nd try/dice/dice.py
@@ -0,0 +1,123 @@
+import random
+
+class Die:
+    """A small cube with a different number of spots on each of its six sides.
+
+    The responsibility of Die is to keep track of the side facing up and calculate the points for 
+    it.
+   
+    Attributes:
+        value (int): The number of spots on the side facing up.
+        points (int): The number of points the die is worth.
+    """
+
+# 2) Create the class constructor. Use the following method comment.
+    def __init__(self):
+        """Constructs a new instance of Die with a value and points attribute.
+        
+            Args:
+                self (Die): An instance of Die.
+         """
+        self.value = 0
+        self.points = 0
+# 3) Create the roll(self) method. Use the following method comment.
+    def roll(self):
+        """Generates a new random value and calculates the points.
+        
+        Args:
+            self (Die): An instance of Die.
+        """
+        self.value = random.randint(1,6)
+        if self.value == 5:
+            self.points = 50
+        elif self.value == 1:
+            self.points = 100
+        else:
+            self.points = 0
+        self.points = 50 if self.value == 5 else 100 if self.value == 1 else 0
+
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        dice (List[Die]): A list of Die instances.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.dice = []
+        self.is_playing = True
+        self.score = 0
+        self.total_score = 0
+
+        for i in range(5):
+            die = Die()
+            self.dice.append(die)
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to roll.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        roll_dice = input("Roll dice? [y/n] ")
+        self.is_playing = (roll_dice == "y")
+       
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            die.roll()
+            self.score += die.points 
+        self.total_score += self.score
+
+    def do_outputs(self):
+        """Displays the dice and the score. Also asks the player if they want to roll again. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+        values = ""
+        for i in range(len(self.dice)):
+            die = self.dice[i]
+            values += f"{die.value} "
+
+        print(f"You rolled: {values}")
+        print(f"Your score is: {self.total_score}\n")
+
+
+
+director = Director()
+director.start_game()
+
