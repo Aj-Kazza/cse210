2fe2e2d9ebebbb67ae2b95ad7ef6e4db3b5470ff
Aj-Kazza <kat21008@byui.edu>
2022-10-04 20:12:00 +1200

    comments an readme

--- 2ndtry/jumper/game/guesser.py
+++ 2ndtry/jumper/game/guesser.py
@@ -1,9 +1,21 @@
 
 class Guesser:
+    """
+    Guesser class: contains the guess, and probably that one thing to make the "_" thing work
+    """
     def __init__(self):
+        """
+        holds the guess variable
+        """
         self._guess = ""
 
+    #got this from youtube, somehow managed to make it work on its own
     def check_letter(self, word, reveal):
+        """
+        checks letter: arg:
+        - checks with word, which is supposed to connect two words._word
+        - connects with separated word list to show correcponding correct letters
+        """
         guess = self._guess
         for i in range(0,len(word)):
             letter = word[i]
@@ -16,5 +28,8 @@ class Guesser:
 
 
     def guess_letter(self):
+        """
+        inputs the guess and holds it as a variable
+        """
         guess = input("Guess a letter [a-z]:    ")
         self._guess = guess
